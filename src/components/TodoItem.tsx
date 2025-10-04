f4d36572f08a2f47b6f4cbd8e99a89800108af1b
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-10-04 18:16:25 +0200

    fix: Remove unused import

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import type { Todo } from "../types/Todo";
-import { TrashIcon, PencilSquareIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { TrashIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   todo: Todo;
